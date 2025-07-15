477ed671837b658e1dd2c495b03208272718a2a9
Marina <marina@estudioelangel.com.ar>
2025-07-15 15:04:50 -0300

    modif api.js, estilos,js y productos para ver las compras con precios en el carrito de los productos desde la api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,20 +1,25 @@
-fetch('https://fakestoreapi.com/products')
+document.addEventListener("DOMContentLoaded",() => {
+ fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
   .then(data => { console.log(data);
-  const product = { title: 'New Product', price: 29.99 };
+  
   const contenedor = document.getElementById ("ejemplos");
     data.forEach(producto => {
     contenedor.innerHTML +=`<div class="card-content">
     <img class="imgApi" src="${producto.image}" alt="${producto.title}"><h3>${producto.title} </h3>
-    <p class="price">Precio: $${producto.price}</p>
+    <p class="price">Precio: ${producto.price}</p>
     <button class="btn boton-agregar" type="button">Comprar</button>
     </div>`    
     });
+        if (typeof iniciarBotones === "function") {
+      iniciarBotones(); 
+    }
     })
     .catch((error)=> console.error("Error al obtener datos de API:", error));
+        //me fijo si la funcion iniciarBotones existe y si es una función la ejecuto
 
-
-
+ 
+});
 
 
 /*fetch('https://fakestoreapi.com/products', {
